d0a3b69614cc68bb725cf478f4f9efb606998da0
rohsen <rohansen2468@gmail.com>
2025-11-02 13:43:52 -0800

    Pop function inplemented

--- heap.h
+++ heap.h
@@ -21,13 +21,21 @@ struct MinHeap {
         }
         data[size] = idx;
         upheap(size, weightArr);
-        size++
+        size++;
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if (size == 0) {
+            cerr << "Heap is empty" << endl;
+            return -1;
+        }
+        int top = data[0];
+        size--;
+        if (size > 0) {
+            data[0] = data[size];
+            downheap(0, weightArr);
+        }
+        return top;
     }
 
     void upheap(int pos, int weightArr[]) {
